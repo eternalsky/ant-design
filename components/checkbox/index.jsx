d279653585b1c2180026ac6cf45376e8d759af50
Leon Shi <superRaytin@gmail.com>
2016-06-07 17:54:06 +0800

    implement shouldComponentUpdate to make the re-render much faster for Checkbox

--- components/checkbox/index.jsx
+++ components/checkbox/index.jsx
@@ -2,11 +2,24 @@ import RcCheckbox from 'rc-checkbox';
 import React from 'react';
 import CheckboxGroup from './Group';
 import classNames from 'classnames';
+import shouldUpdate from '../_util/shouldUpdate';
 
 export default class Checkbox extends React.Component {
   static Group = CheckboxGroup;
   static defaultProps = {
     prefixCls: 'ant-checkbox',
+  }
+  shouldComponentUpdate(nextProps) {
+    const checkProps = [
+      'disabled',
+      'checked',
+      'prefixCls',
+      'children',
+      'style',
+      'type',
+      'className',
+    ];
+    return shouldUpdate(checkProps, this.props, nextProps);
   }
   render() {
     const { prefixCls, style, children, className, ...restProps } = this.props;
